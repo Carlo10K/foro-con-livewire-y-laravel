b8d8fb404bdbdc1903e1fc848a82b62820ecfaec
Carlo10K <carlosortegadiaz50@gmail.com>
2023-09-26 20:07:45 -0600

    ingreso de respuesta

--- app/Livewire/ShowThread.php
+++ app/Livewire/ShowThread.php
@@ -8,6 +8,21 @@ use Livewire\Component;
 class ShowThread extends Component
 {
     public Thread $thread;
+    public $body = '';
+
+    public function postReply()
+    {
+        //validar
+        $this->validate(['body' => 'required']);
+
+        //crear
+        auth()->user()->replies()->create([
+            'thread_id' => $this->thread->id,
+            'body' => $this->body
+        ]);
+        //refresh
+        $this->body = '';
+    }
 
     public function render()
     {
