c15ae02eeb9f722599c98b1463fbfdac227ea9ed
Carlo10K <carlosortegadiaz50@gmail.com>
2023-10-02 18:27:51 -0600

    editar pregunta

---
+++ resources/views/thread/form.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <select name="category_id" class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs capitalize mb-4">
+        <option value="">Seleccionar categoría</option>
+    </select>
+</div>
