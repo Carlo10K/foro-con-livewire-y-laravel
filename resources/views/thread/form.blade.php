ef99f571779acc1fbc16a4907af7198550ae4619
Carlo10K <carlosortegadiaz50@gmail.com>
2023-10-03 09:34:36 -0600

    update de los threads

--- resources/views/thread/form.blade.php
+++ resources/views/thread/form.blade.php
@@ -1,5 +1,15 @@
 <div>
     <select name="category_id" class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs capitalize mb-4">
         <option value="">Seleccionar categoría</option>
+
+        @foreach ($categories as $category)
+            <option value="{{ $category->id }}" @selected(old('category_id',  $thread->category_id) == $category->id)>{{ $category->name }}</option>
+        @endforeach
     </select>
+
+    <input type="text" name="title" placeholder="Título" class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs mb-4" value="{{ old('title', $thread->title) }}">
+
+    <textarea name="body" rows="10" placeholder="Descripcion del problema" class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs mb-4">
+    {{ old('body', $thread->body ) }}
+    </textarea>
 </div>
